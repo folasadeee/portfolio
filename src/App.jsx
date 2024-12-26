d8d9e14f0cb9982551da7deafeed083416c773a4
folasade <folasade.goddard@gmail.com>
2024-12-25 23:25:19 -0500

    Initialized Tailwind CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-        <h1>Hi! My name is Folasade.</h1>
+        <h1 className="text-3xl font-bold">Hi! My name is Folasade.</h1>
         <h2>I am a full-stack developer.</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et  egestas velit, sed consequat nisi. Nunc iaculis consequat lacinia.  Integer blandit dolor eget urna porta lobortis. In eget velit eu erat  ultricies efficitur at sit amet purus. Maecenas pharetra venenatis mi,  vel porttitor elit consequat nec. Vestibulum sit amet posuere nulla. Sed varius metus tortor, vel pretium quam commodo et. Fusce ultricies nisl  ac neque consequat dictum. Vivamus vitae ex justo. Nulla convallis felis non massa eleifend, quis dictum nulla suscipit. Mauris ac tincidunt  felis.</p>
 
