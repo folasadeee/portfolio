40c0c81dd637d4030ded524fbac0b5f2ead321ab
folasade <folasade.goddard@gmail.com>
2024-12-25 22:45:41 -0500

    Added CSS variables and custom font faces

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,10 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>Hi! My name is Folasade.</h1>
+        <h2>I am a full-stack developer.</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et  egestas velit, sed consequat nisi. Nunc iaculis consequat lacinia.  Integer blandit dolor eget urna porta lobortis. In eget velit eu erat  ultricies efficitur at sit amet purus. Maecenas pharetra venenatis mi,  vel porttitor elit consequat nec. Vestibulum sit amet posuere nulla. Sed varius metus tortor, vel pretium quam commodo et. Fusce ultricies nisl  ac neque consequat dictum. Vivamus vitae ex justo. Nulla convallis felis non massa eleifend, quis dictum nulla suscipit. Mauris ac tincidunt  felis.</p>
+
     </>
   )
 }
